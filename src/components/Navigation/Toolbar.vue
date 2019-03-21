378bf06c1b0f6f7d8aac28eecf67cd342a1edf3b
James Littlefield <jimilittle3@hotmail.com>
2019-03-21 11:42:58 -0500

    toolbar styling

--- src/components/Navigation/Toolbar.vue
+++ src/components/Navigation/Toolbar.vue
@@ -1,22 +1,17 @@
 <template>
-    <div>
-        <v-toolbar class="t" color="white">
-    <v-btn
-        color=""
+  <div>
+    <v-toolbar class="t" color="white">
+      <v-btn
+        class="b"
+        large
+        fab
         flat
         value="recent"
-        to="/comp1"
-      >
+        to="/">
         <v-icon color="white">home</v-icon>
       </v-btn>
-    <v-toolbar-title>Title</v-toolbar-title>
-    <v-spacer></v-spacer>
-    <v-toolbar-items class="hidden-sm-and-down">
-      <v-btn flat>Link One</v-btn>
-      <v-btn flat>Link Two</v-btn>
-      <v-btn flat>Link Three</v-btn>
-    </v-toolbar-items>
-  </v-toolbar>
+      <v-toolbar-title>TITLE</v-toolbar-title>
+    </v-toolbar>
     </div>
 </template>
 
@@ -25,5 +20,8 @@
     color: white !important;
     background-color: #1867C0 !important;
     border-bottom: 1px solid black !important;
+}
+.b:hover, .b:before {
+  color: transparent;
 }
 </style>
