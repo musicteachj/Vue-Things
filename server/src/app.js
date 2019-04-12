95a10037ec1d728d57da9d5553617f4f8a51e7a2
James Littlefield <jimilittle3@hotmail.com>
2019-04-11 20:53:26 -0500

    connect to mongodb

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,32 @@
-console.log('Hello from  server');
+// const express = require('express')
+// const bodyParser = require('body-parser')
+// const cors = require('cors')
+// const morgan = require('morgan')
+// const app = express()
+// app.use(morgan('combined'))
+// app.use(bodyParser.json())
+// app.use(cors())
+// app.set('port', process.env.PORT || 8081);
+// app.listen(process.env.PORT || 8081)
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 4000;
+const cors = require('cors');
+const mongoose = require('mongoose');
+const config = require('../api/DB.js');
+
+mongoose.Promise = global.Promise;
+mongoose.connect(config.DB, { useNewUrlParser: true }).then(
+  () => {console.log('Database is connected') },
+  err => { console.log('Can not connect to the database'+ err)}
+);
+
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.listen(PORT, function(){
+  console.log('Server is running on Port:',PORT);
+});
